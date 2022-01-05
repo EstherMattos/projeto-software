388a6c96629ffc8eb666365cd8778ba22ccd06c9
Brendan Buckley <brbuckley@id.uff.br>
2022-01-05 10:13:38 -0300

    Firebase
    firebase e hooks instalados e tentativa de login com google

--- src/View/App.js
+++ src/View/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDQtQtQiIjDUs2oNd0aFYsLT97yrgGYToo",
+  authDomain: "wings-d53c0.firebaseapp.com",
+  projectId: "wings-d53c0",
+  storageBucket: "wings-d53c0.appspot.com",
+  messagingSenderId: "693489427088",
+  appId: "1:693489427088:web:7a45e080aff79f42572e15"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  
+  
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/View/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Teste</h1>
       </header>
+
+      <section>
+        {user ? <Logado /> : <SignIn />}
+      </section>
+
     </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+    </>
+  )
+
+}
+
+function Logado() {
+
+  return (
+    <>
+      <p>Logado com sucesso pelo google</p>
+    </>
+  )
+
 }
 
 export default App;
