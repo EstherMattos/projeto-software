c4827033606ad28431937c48fb720aa0da1777f4
CastroFabio <fmpc95@gmail.com>
2022-01-09 14:25:26 -0300

    Signin, page e component, criados, assim como components form input, custom button e signin

--- src/View/App.js
+++ src/View/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
