e5aa53845cae90339b9d9ef4d3ebe3f4656b9f65
CastroFabio <fmpc95@gmail.com>
2022-01-09 14:12:33 -0300

    adicionando Header component lnkado a Home e shop

--- src/View/App.js
+++ src/View/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
