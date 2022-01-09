00d54489853719014509e1302f0ce9c72074350a
CastroFabio <fmpc95@gmail.com>
2022-01-09 13:23:41 -0300

    atualizando menu-item com withRouter

--- src/View/components/menu-item/menu-item.component.jsx
+++ src/View/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,17 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router';
 
-import "./menu-item.styles.scss";
+import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
     const navigate = useNavigate();
 
     return (
-        <div className={`${size} menu-item`} onClick={() => navigate(`hats`)}>
+        <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
             <div
                 className="background-image"
-                style={{
-                    backgroundImage: `url(${imageUrl})`,
-                }}
-            ></div>
-
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
